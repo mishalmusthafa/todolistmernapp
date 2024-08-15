7db2ffaf111589b942fd2c754f75ed6cad226ea6
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-08-15 12:09:36 +0530

    Added the new backend service url with the railway

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://todolistmernapp-backend.onrender.com/api/users';
+const API_URL = 'https://todolistmernapp-production.up.railway.app/api/users';
 console.log('apiurl', API_URL);
 
 //Register user
